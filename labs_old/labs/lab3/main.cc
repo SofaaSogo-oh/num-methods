57383232d45a067e1e8b85e6f1561abc4a592f99
SofaaSogo(oh) <NaumikhinDD@stud.kai.ru>
2025-03-29 17:19:56 +0300

    r3

--- labs_old/labs/lab3/main.cc
+++ labs_old/labs/lab3/main.cc
@@ -4,29 +4,64 @@
 #include <tuple>
 #include <utility>
 
+const auto &&tmp_fill_cell = [](char c) {
+  return ostream_invoker{[c](std::ostream &os) -> std::ostream & {
+    os << std::setfill(c) << c << std::setfill(' ');
+    return os;
+  }};
+};
+
+const auto &&fill_cell = tmp_fill_cell('-');
+const auto &&hard_fill_cell = tmp_fill_cell('=');
+
+const auto &&table_header = row_wrapper("k", "x_k", "x_k-1", "dx", "dy");
+const auto &&table_line =
+    row_wrapper(fill_cell, fill_cell, fill_cell, fill_cell, fill_cell);
+const auto &&table_hard_line =
+    row_wrapper(hard_fill_cell, hard_fill_cell, hard_fill_cell, hard_fill_cell,
+                hard_fill_cell);
+
 using R3 = std::tuple<double, double>;
 
 template <typename... T>
 auto operator-(const std::tuple<T...> &t1, const std::tuple<T...> &t2) {
-  return std::apply(
-      [t1, t2](auto &&...inx) {
+  return std::invoke(
+      [t1, t2]<size_t... inx>(std::index_sequence<inx...>) {
         return std::make_tuple(std::get<inx>(t1) - std::get<inx>(t2)...);
       },
       std::index_sequence_for<T...>());
 }
 
-template <typename... T> auto maxs(const std::tuple<T...> &seq) {
+template <typename... T>
+std::ostream &operator<<(std::ostream &os, const std::tuple<T...> &t) {
   return std::apply(
-      [](auto &&car, auto &&...cdr) {
-        return ((car = std::max(cdr, car)), ...);
+      [&os]<typename... U>(U &&...args) -> std::ostream & {
+        return os << "(",
+               (os << ...
+                   << ostream_invoker([arg = std::forward<U>(args)](
+                                          std::ostream &os) -> std::ostream & {
+                        return os << arg << " ";
+                      }))
+                   << ")";
       },
-      seq);
+      t);
 }
 
+template <typename... T> auto maxs(const std::tuple<T...> &seq) {
+  return std::apply([](auto &&...args) { return std::max({args...}); }, seq);
+}
+
+#if 1
 auto sim(R3 init_aprox, double eps) {
   return [init_aprox, eps](std::ostream &os) -> std::tuple<size_t, R3, double> {
-    auto f = [](double x, double y) -> double {};
-    auto g = [](double x, double y) -> double {};
+    auto f = [](double x, double y) -> double {
+      return x + ((-x) / (2. * x - y)) * (2. * x + y - 7.) +
+             (1. / (2. * x * y)) * (x * y - 7);
+    };
+    auto g = [](double x, double y) -> double {
+      return y + ((-y) / (y - 2. * x)) * (2. * x + y - 7.) +
+             (2. / (y - 2. * x)) * (x * y - 7);
+    };
     size_t k;
     R3 x_k = init_aprox;
     for (k = 0;; ++k) {
@@ -38,10 +73,6 @@ auto sim(R3 init_aprox, double eps) {
     return {k, x_k, eps};
   };
 }
+#endif
 
-int main() {
-  try_block([] {
-    std::cout << ostream_invoker{[](std::ostream &os) -> std::ostream & {}};
-  });
-  return 0;
-}
+int main() { return 0; }
