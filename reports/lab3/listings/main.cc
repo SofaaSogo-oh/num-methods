d4f2e0c096aa8dd4b4f98fa7893462db1e75c048
SofaaSogo(oh) <NaumikhinDD@stud.kai.ru>
2025-03-31 16:54:54 +0300

    sim report

--- reports/lab3/listings/main.cc
+++ reports/lab3/listings/main.cc
@@ -1,61 +1,76 @@
-inline constexpr auto argument_check = [](const R3 &init_aprx) -> void {
-  if (!(-1 <= init_aprx && init_aprx <= -.5))
-    throw std::invalid_argument("Given argument should be in [-1, -1/2]");
+auto &&argument_check = [](double x, double y) {
+  if (!((std::abs(3 * (y - 3) * (y - 4)) < (2 * x - y) * (2 * x - y)) &&
+        (std::abs(3 * (2 * x - 3) * (x - 2)) < (2 * x - y) * (2 * x - y))))
+    throw std::invalid_argument(std::invoke([&]() {
+      std::stringstream ss;
+      ss << "Given argument is incorrect, it should be in this set:"
+         << std::endl;
+      ss << "{3|(y-3)(y-4)| < (2x-y)^2" << std::endl;
+      ss << "{3|(2x-3)(x-2)| < (2x-y)^2" << std::endl;
+      ss << "Given argument is: " << R3{x, y} << std::endl;
+      return ss.str();
+    }));
 };
 
-decltype(auto) sim(const R3 &init_aprx, double eps) {
-  return [&init_aprx = std::as_const(init_aprx),
-          eps](std::ostream &os) -> std::tuple<size_t, R3, double> {
-    argument_check(init_aprx);
-    auto &&f = [](double x) { return std::exp(2. * x) + x * x * x; };
-    auto &&h = [](double x) {
-      return x - (std::exp(2. * x) + (x * x * x)) / 2.;
+double f0(double x, double y) { return 2 * x + y - 7; }
+
+double f1(double x, double y) { return x * y - 6; }
+
+auto sim(const R3 &init_aprox, double eps) {
+  return [init_aprox, eps](std::ostream &os) -> method_result {
+    std::apply(argument_check, init_aprox.to_tuple());
+    auto &&f = [](double x, double y) -> double {
+      return x - ((2 * x - 3) * (x - 2)) / (2 * x - y);
     };
-    R3 x_k = init_aprx;
+    auto &&g = [](double x, double y) -> double {
+      return y + ((y - 3) * (y - 4)) / (2 * x - y);
+    };
+    auto &&next = [&f, &g](const R3 &cur) -> R3 {
+      auto &&tup = cur.to_tuple();
+      return {std::apply(f, tup), std::apply(g, tup)};
+    };
+    auto &&ev_f = [](auto &&func, R3 x_k) -> double {
+      return std::abs(std::apply(func, x_k.to_tuple()));
+    };
     size_t k;
+    R3 x_k = init_aprox;
     for (k = 0;; ++k) {
-      auto &&prev = std::exchange(x_k, h(x_k));
-      auto &&dx = std::abs(x_k - prev), dy = f(x_k);
-      os << row_wrapper{k, x_k, prev, dx, dy} << std::endl;
-      if (dx < eps && std::abs(dy) < eps)
+      auto &&prev = std::exchange(x_k, next(x_k));
+      os << std::apply([](auto &&...args) { return row_wrapper(args...); },
+                       convert_to_row(k, x_k, prev));
+      throw_any_nan(x_k);
+      if (Rnu::norm(x_k - prev) < eps &&
+          (ev_f(f0, x_k) < eps && ev_f(f1, x_k) < eps))
         break;
     }
     return {k, x_k, eps};
   };
 }
 
-decltype(auto) newton(const R3 &init_aprx, double eps) {
-  return [&init_aprx = std::as_const(init_aprx),
-          eps](std::ostream &os) -> std::tuple<size_t, R3, double> {
-    argument_check(init_aprx);
-    auto &&f = [](double x) { return std::exp(2. * x) + x * x * x; };
-    auto &&g = [](double x) { return 2. * std::exp(2. * x) + 3. * x * x; };
+auto newthon(const R3 &init_aprx, double eps) {
+  return [init_aprx, eps](std::ostream &os) -> method_result {
+    auto &&f = [](double x, double y) -> double {
+      return x - (x * f0(x, y) - f1(x, y)) / (2 * x - y);
+    };
+    auto &&g = [](double x, double y) -> double {
+      return y - (-y * f0(x, y) + 2 * f1(x, y)) / (2 * x - y);
+    };
+    auto &&next = [&f, &g](const R3 &cur) -> R3 {
+      auto &&tup = cur.to_tuple();
+      return {std::apply(f, tup), std::apply(g, tup)};
+    };
+    auto &&ev_f = [](auto &&func, const R3 &cur) {
+      return std::abs(std::apply(func, cur.to_tuple()));
+    };
+    size_t k;
     R3 x_k = init_aprx;
-    size_t k;
     for (k = 0;; ++k) {
-      auto &&prev = std::exchange(x_k, x_k - f(x_k) / g(x_k));
-      auto &&dx = std::abs(x_k - prev), dy = f(x_k);
-      os << row_wrapper{k, x_k, prev, dx, dy} << std::endl;
-      if (dx < eps && std::abs(dy) < eps)
-        break;
-    }
-    return {k, x_k, eps};
-  };
-}
-
-decltype(auto) mod_newton(const R3 &init_aprx, double eps) {
-  return [&init_aprx = std::as_const(init_aprx),
-          eps](std::ostream &os) -> std::tuple<size_t, R3, double> {
-    argument_check(init_aprx);
-    auto &&f = [](double x) { return std::exp(2. * x) + x * x * x; };
-    auto &&g = [](double x) { return 2. * std::exp(2. * x) + 3. * x * x; };
-    R3 x_k = init_aprx, g_0 = g(init_aprx);
-    size_t k;
-    for (k = 0;; ++k) {
-      auto &&prev = std::exchange(x_k, x_k - f(x_k) / g_0);
-      auto &&dx = std::abs(x_k - prev), dy = f(x_k);
-      os << row_wrapper{k, x_k, prev, dx, dy} << std::endl;
-      if (dx < eps && std::abs(dy) < eps)
+      auto &&prev = std::exchange(x_k, next(x_k));
+      os << std::apply([](auto &&...args) { return row_wrapper(args...); },
+                       convert_to_row(k, x_k, prev));
+      throw_any_nan(x_k);
+      if (Rnu::norm(x_k - prev) < eps &&
+          (ev_f(f0, x_k) < eps && ev_f(f1, x_k) < eps))
         break;
     }
     return {k, x_k, eps};
